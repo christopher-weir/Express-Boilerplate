73a1cd418c154dc491ba62789aa9aa3878629493
Christopher Weir <christopher_weir@me.com>
2015-08-04 18:12:10 +0100

    build files

--- public/js/app.js
+++ public/js/app.js
@@ -253,6 +253,52 @@ e.$validators.maxlength=function(a,c){return 0>f||e.$isEmpty(c)||c.length<=f}}}}
 
 'use strict';
 
+
+// Init the application configuration module for AngularJS application
+var ApplicationConfiguration = (function() {
+    // Init module configuration options
+    var applicationModuleName = 'ExpressBoilerplate';
+    var applicationModuleVendorDependencies = [];
+
+    // Add a new vertical module
+    var registerModule = function(moduleName, dependencies) {
+        // Create angular module
+        angular.module(moduleName, dependencies || []);
+
+        // Add the module to the AngularJS configuration file
+        angular.module(applicationModuleName).requires.push(moduleName);
+    };
+
+    return {
+        applicationModuleName: applicationModuleName,
+        applicationModuleVendorDependencies: applicationModuleVendorDependencies,
+        registerModule: registerModule
+    };
+})();
+
+'use strict';
+
+//Start by defining the main module and adding the module dependencies
+angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+
+// Setting HTML5 Location Mode
+angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
+    function($locationProvider) {
+        $locationProvider.html5Mode(true).hashPrefix('!');
+    }
+]);
+
+//Then define the init function for starting up the application
+angular.element(document).ready(function() {
+    //Fixing facebook bug with redirect
+    if (window.location.hash === '#_=_') window.location.hash = '#!';
+
+    //Then init the app
+    angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+});
+
+'use strict';
+
 // Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('core');
 ApplicationConfiguration.registerModule('templates-main');
