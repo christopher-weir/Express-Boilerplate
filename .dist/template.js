18257e352933e227da1d3a48e716a4dcc7a35930
Christopher Weir <christopher_weir@me.com>
2015-08-05 16:27:06 +0100

    build files

--- .dist/template.js
+++ .dist/template.js
@@ -3,19 +3,7 @@ try { module = angular.module("templates-main"); }
 catch(err) { module = angular.module("templates-main", []); }
 module.run(["$templateCache", function($templateCache) {
   "use strict";
-  $templateCache.put("modules/footer/client/views/footer.client.view.html",
-    "<h1>footer is working</h1>\n" +
-    "\n" +
-    "<p>ebd footer</p>");
-}]);
-})();
-
-(function(module) {
-try { module = angular.module("templates-main"); }
-catch(err) { module = angular.module("templates-main", []); }
-module.run(["$templateCache", function($templateCache) {
-  "use strict";
-  $templateCache.put("modules/phonebook/client/views/phonebook.client.view.html",
-    "<h1>phonebook is working awefawe faw fa w awef </h1>");
+  $templateCache.put("modules/newtest/client/views/newtest.client.view.html",
+    "<h1>newtest is working</h1>");
 }]);
 })();
--- public/js/app.js
+++ public/js/app.js
@@ -304,120 +304,18 @@ ApplicationConfiguration.registerModule('core');
 ApplicationConfiguration.registerModule('templates-main');
 'use strict';
 
-// Use Applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('footer');
-'use strict';
-
-// Use Applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('phonebook');
-'use strict';
-
 angular.module('core').controller('CoreCtrl', [
     '$scope',
     function( $scope ) {
         console.log('test');
     }
 ]);
-'use strict';
-
-angular.module('footer').controller('footerCtrl', [
-    '$scope',
-    function( $scope ) {
-        console.log('footer is working');
-    }
-]);
-'use strict';
-
-angular.module('footer')
-    .directive('footer', [
-        function( ) {
-            return {
-
-                restrict: 'A',
-
-                controller  : 'footerCtrl',
-
-                templateUrl: 'modules/footer/client/views/footer.client.view.html'
-
-            };
-        }
-    ]
-);
-'use strict';
-
-
-angular.module('footer')
-    .factory('footerService', [
-        function () {
-
-            return {
-
-                testfooter: function () {
-                    return 'footer';
-                }
-
-
-            };
-        }
-    ]);
-'use strict';
-
-angular.module('phonebook').controller('phonebookCtrl', [
-    '$scope',
-    function( $scope ) {
-        console.log('phonebook is working');
-    }
-]);
-'use strict';
-
-angular.module('phonebook')
-    .directive('phonebook', [
-        function( ) {
-            return {
-
-                controller  : 'phonebookCtrl',
-
-                templateUrl: 'modules/phonebook/client/views/phonebook.client.view.html'
-
-            };
-        }
-    ]
-);
-'use strict';
-
-
-angular.module('phonebook')
-    .factory('phonebookService', [
-        function () {
-
-            return {
-
-                testphonebook: function () {
-                    return 'phonebook';
-                }
-
-
-            };
-        }
-    ]);
 (function(module) {
 try { module = angular.module("templates-main"); }
 catch(err) { module = angular.module("templates-main", []); }
 module.run(["$templateCache", function($templateCache) {
   "use strict";
-  $templateCache.put("modules/footer/client/views/footer.client.view.html",
-    "<h1>footer is working</h1>\n" +
-    "\n" +
-    "<p>ebd footer</p>");
-}]);
-})();
-
-(function(module) {
-try { module = angular.module("templates-main"); }
-catch(err) { module = angular.module("templates-main", []); }
-module.run(["$templateCache", function($templateCache) {
-  "use strict";
-  $templateCache.put("modules/phonebook/client/views/phonebook.client.view.html",
-    "<h1>phonebook is working awefawe faw fa w awef </h1>");
+  $templateCache.put("modules/newtest/client/views/newtest.client.view.html",
+    "<h1>newtest is working</h1>");
 }]);
 })();
