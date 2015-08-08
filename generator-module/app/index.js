65712bccc09a8e19221a6ad546224ee3d6481e86
Christopher Weir <christopher_weir@me.com>
2015-08-08 13:59:44 +0100

    updated module var name

--- generator-module/app/index.js
+++ generator-module/app/index.js
@@ -11,12 +11,12 @@ module.exports = generators.Base.extend({
         console.log(this.yeoman);
 
         var prompts = [{
-                name: 'appName',
+                name: 'moduleName',
                 message: 'What is your modules name ?'
         }];
 
         this.prompt(prompts, function (props) {
-            this.appName = props.appName;
+            this.moduleName = props.moduleName;
             done();
         }.bind(this));
     },
@@ -24,59 +24,59 @@ module.exports = generators.Base.extend({
         //console.log(this.sourceRoot());
         //console.log(this.destinationRoot());
 
-        this.mkdir('modules/' + this.appName);
+        this.mkdir('modules/' + this.moduleName);
         // client folders
-        this.mkdir('modules/' + this.appName + '/client');
-        this.mkdir('modules/' + this.appName + '/client/controllers');
-        this.mkdir('modules/' + this.appName + '/client/directives');
-        this.mkdir('modules/' + this.appName + '/client/views');
-        this.mkdir('modules/' + this.appName + '/client/services');
+        this.mkdir('modules/' + this.moduleName + '/client');
+        this.mkdir('modules/' + this.moduleName + '/client/controllers');
+        this.mkdir('modules/' + this.moduleName + '/client/directives');
+        this.mkdir('modules/' + this.moduleName + '/client/views');
+        this.mkdir('modules/' + this.moduleName + '/client/services');
         // server folders
-        this.mkdir('modules/' + this.appName + '/server');
-        this.mkdir('modules/' + this.appName + '/server/controllers');
-        this.mkdir('modules/' + this.appName + '/server/routes');
+        this.mkdir('modules/' + this.moduleName + '/server');
+        this.mkdir('modules/' + this.moduleName + '/server/controllers');
+        this.mkdir('modules/' + this.moduleName + '/server/routes');
         // done();
     },
     copyMainFiles: function(){
 
         var context = {
-            module_name: this.appName
+            module_name: this.moduleName
         };
 
         this.template(
             '__module.client.module.js',
-            'modules/' + this.appName + '/client/' + this.appName + '.client.module.js', context);
+            'modules/' + this.moduleName + '/client/' + this.moduleName + '.client.module.js', context);
 
         this.template(
             '__controller.client.controller.js',
-            'modules/' + this.appName + '/client/controllers/' + this.appName + '.client.controller.js', context);
+            'modules/' + this.moduleName + '/client/controllers/' + this.moduleName + '.client.controller.js', context);
 
         this.template(
             '__directive.client.directive.js',
-            'modules/' + this.appName + '/client/directives/' + this.appName + '.client.directive.js', context);
+            'modules/' + this.moduleName + '/client/directives/' + this.moduleName + '.client.directive.js', context);
 
         this.template(
             '__view.client.view.html',
-            'modules/' + this.appName + '/client/views/' + this.appName + '.client.view.html', context);
+            'modules/' + this.moduleName + '/client/views/' + this.moduleName + '.client.view.html', context);
 
         this.template(
             '__service.client.service.js',
-            'modules/' + this.appName + '/client/services/' + this.appName + '.client.service.js', context);
+            'modules/' + this.moduleName + '/client/services/' + this.moduleName + '.client.service.js', context);
 
 
 
 
         this.template(
             '___controller.server.controller.js',
-            'modules/' + this.appName + '/server/controllers/' + this.appName + '.server.controller.js', context);
+            'modules/' + this.moduleName + '/server/controllers/' + this.moduleName + '.server.controller.js', context);
 
         this.template(
             '___routes.server.routes.js',
-            'modules/' + this.appName + '/server/routes/' + this.appName + '.server.routes.js', context);
+            'modules/' + this.moduleName + '/server/routes/' + this.moduleName + '.server.routes.js', context);
 
         this.template(
             '___view.server.view.html',
-            'modules/' + this.appName + '/server/views/' + this.appName + '.server.view.html', context);
+            'modules/' + this.moduleName + '/server/views/' + this.moduleName + '.server.view.html', context);
     },
 });
 
