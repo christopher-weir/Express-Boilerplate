bd425707441bd97efe14a3f7eb112b73c48a1b5b
Christopher Weir <christopher_weir@me.com>
2016-01-15 10:44:17 +0000

    cleanup for react prep

--- config/config.js
+++ config/config.js
@@ -79,7 +79,7 @@ var initGlobalConfigFolders = function(config, assets) {
     };
 
     // Setting globbed client paths
-    config.folders.client = getGlobbedPaths(path.join(process.cwd(), 'modules/*/client/'), process.cwd().replace(new RegExp(/\\/g),'/'));
+    config.folders.client = getGlobbedPaths(path.join(process.cwd(), 'modules/*/client/'), process.cwd().replace(new RegExp(/\\/g), '/'));
 };
 
 /**
