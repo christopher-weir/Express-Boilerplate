3a3a3c3194502a395f759e94908f89698bed3cf2
Christopher Weir <christopher_weir@me.com>
2016-01-22 15:25:30 +0000

    Removed shared folder

--- modules/core/shared/core.shared.module.js
+++
@@ -1,7 +0,0 @@
-// register components
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var newtest = require('./test.js');
-
-ReactDOM.render(React.createElement(newtest), document.getElementById('examplehelloMessagea'));
