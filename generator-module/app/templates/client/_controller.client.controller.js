4a563410c8eee30e9fbabdc7c57cd9be4cbbad87
Christopher Weir <christopher_weir@me.com>
2016-04-07 15:03:21 +0100

    Removed the old angular yo

--- generator-module/app/templates/client/_controller.client.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('<%= module_name %>').controller('<%= module_name %>Ctrl', [
-    '$scope',
-    function( $scope ) {
-        console.log('<%= module_name %> is working');
-    }
-]);
